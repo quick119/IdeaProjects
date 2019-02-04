12ba160c21c1ff911a07065fb79015579d41e971
Simon Wang <quick119@hotmail.com>
2019-02-04 23:11:18 +0800

    單元25

--- src/main/java/com/quick/parking/Tester.java
+++ src/main/java/com/quick/parking/Tester.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
@@ -12,9 +13,20 @@ import java.util.Locale;
 
 public class Tester {
     public static void main(String[] args) {
-        //java8
-        java8();
-        //java();
+        LocalDateTime enter =
+                LocalDateTime.of(2018, 12, 25,
+                        8, 0, 0);
+        LocalDateTime leave =
+                LocalDateTime.of(2018, 12, 25,
+                        10, 8, 0);
+        Car car = new Car("AA-0001", enter);
+        car.setLeave(leave);
+        System.out.println(car.getDuration());
+        long hours = (long)Math.ceil(car.getDuration()/60.0);
+        System.out.println(hours);
+        System.out.println(30*hours);
+//        java8();
+//        java();
     }
 
     private static void java8() {
