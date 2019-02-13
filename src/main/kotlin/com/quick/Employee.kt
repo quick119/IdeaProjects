06c15a9418ef38a3b10f93e21f45fc1435b62f64
Simon Wang <quick119@hotmail.com>
2019-02-13 15:18:23 +0800

    解構賦值Destructuring Assignment

--- src/main/kotlin/com/quick/Employee.kt
+++ src/main/kotlin/com/quick/Employee.kt
@@ -7,7 +7,7 @@ fun main() {
     val name = emp.component1()
     val age = emp.component2()
     val w = emp.component3()
-//    var (name, age) = Employee("Jack", 30, 80.5f)        //原本使用上面四行才能賦值，可用左側一行處理完成
+//    var (name, age, weight) = Employee("Jack", 30, 80.5f)        //原本使用上面四行才能賦值，可用左側一行處理完成
 
     println("$name $age")
 }
